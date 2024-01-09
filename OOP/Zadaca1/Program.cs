17cf2bd37bf21daebf1199b6fdd6c12aa67a9a9f
fcica <filip.cica@barrage.net>
2024-01-09 21:05:02 +0100

    Add zadaca 1

---
+++ OOP/Zadaca1/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Zadaca1
+{
+    // Bazna klasa Osoba
+    public abstract class Osoba
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int ID;
+
+        public Osoba(string ime, string prezime, int id)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            ID = new Random().Next();
+        }
+    }
+
+    // Klasa Student nasljeđuje Osoba
+    public class Student : Osoba
+    {
+        public List<int> Ocjene { get; set; }
+        public string Razred { get; set; }
+
+        public Student(string ime, string prezime, int id, string razred) : base(ime, prezime, id)
+        {
+            Ocjene = new List<int>();
+            Razred = razred;
+        }
+
+        public void DodajOcjenu(int ocjena)
+        {
+            Ocjene.Add(ocjena);
+        }
+
+        public void PrikaziOcjene()
+        {
+            Console.WriteLine($"Ocjene studenta {Ime} {Prezime}: {string.Join(", ", Ocjene)}");
+        }
+    }
+
+    // Klasa Učitelj nasljeđuje Osoba
+    public class Ucitelj : Osoba
+    {
+        public List<string> Predmeti { get; set; }
+
+        public Ucitelj(string ime, string prezime, int id, List<string> predmeti) : base(ime, prezime, id)
+        {
+            Predmeti = predmeti;
+        }
+
+        public void DodajOcjenuStudentu(Student student, int ocjena)
+        {
+            student.DodajOcjenu(ocjena);
+        }
+
+        public void PregledOcjenaUcenika(Student student)
+        {
+            student.PrikaziOcjene();
+        }
+
+        public void PregledOcjenaRazreda(List<Student> razred)
+        {
+            foreach (var student in razred)
+            {
+                PregledOcjenaUcenika(student);
+            }
+        }
+    }
+
+    // Klasa Ravnatelj nasljeđuje Osoba
+    public class Ravnatelj : Osoba
+    {
+        public List<Ucitelj> Ucitelji { get; set; }
+        public List<Student> Studenti { get; set; }
+
+        public Ravnatelj(string ime, string prezime, int id) : base(ime, prezime, id)
+        {
+            Ucitelji = new List<Ucitelj>();
+            Studenti = new List<Student>();
+        }
+
+        public void DodajUcitelja(Ucitelj ucitelj)
+        {
+            Ucitelji.Add(ucitelj);
+        }
+
+        public void DodajStudenta(Student student)
+        {
+            Studenti.Add(student);
+        }
+
+        public void UkloniUcitelja(Ucitelj ucitelj)
+        {
+            Ucitelji.Remove(ucitelj);
+        }
+
+        public void UkloniStudenta(Student student)
+        {
+            Studenti.Remove(student);
+        }
+
+        public void PregledOpćihOcjenaSkole()
+        {
+            foreach (var student in Studenti)
+            {
+                student.PrikaziOcjene();
+            }
+        }
+    }
+
+    // Klasa Razred
+    public class Razred
+    {
+        public string NazivRazreda { get; set; }
+        public List<Student> Studenti { get; set; }
+
+        public Razred(string nazivRazreda)
+        {
+            NazivRazreda = nazivRazreda;
+            Studenti = new List<Student>();
+        }
+
+        public void DodajStudenta(Student student)
+        {
+            Studenti.Add(student);
+        }
+
+        public void UkloniStudenta(Student student)
+        {
+            Studenti.Remove(student);
+        }
+
+        public void PrikaziOcjeneRazreda()
+        {
+            foreach (var student in Studenti)
+            {
+                student.PrikaziOcjene();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Primjer upotrebe
+            Ravnatelj ravnatelj = new Ravnatelj("ImeRavnatelja", "PrezimeRavnatelja", 1);
+
+            Ucitelj ucitelj = new Ucitelj("ImeUcitelja", "PrezimeUcitelja", 2, new List<string> { "Matematika", "Hrvatski" });
+            ravnatelj.DodajUcitelja(ucitelj);
+
+            Student student = new Student("ImeStudenta", "PrezimeStudenta", 3, "1A");
+            ravnatelj.DodajStudenta(student);
+
+            Razred razred1A = new Razred("1A");
+            razred1A.DodajStudenta(student);
+
+            ucitelj.DodajOcjenuStudentu(student, 5);
+
+            ravnatelj.PregledOpćihOcjenaSkole();
+            ucitelj.PregledOcjenaRazreda(razred1A.Studenti);
+        }
+    }
+}
---
+++ OOP/Zadaca1/Readme.md
@@ -0,0 +1,51 @@
+﻿#Prva Zadaca
+Zadatak: Aplikacija za Praćenje Ocjena Studenata
+Cilj zadatka je razviti aplikaciju za praćenje ocjena studenata u školskom sustavu koristeći principima objektno orijentiranog programiranja (OOP) u programskom jeziku C#.
+
+Klase:
+Osoba:
+Atributi: Ime, Prezime, ID
+Metode: Konstruktor, Get/Set metode za atribute
+
+Student:
+Atributi: Ocjene, Razred
+Metode: Konstruktor, Metoda za dodavanje ocjene, Metoda za prikazivanje ocjena
+
+Učitelj:
+Atributi: Predmeti kojima predaje
+Metode: Konstruktor, Metoda za dodavanje ocjena studentima, Metoda za pregled ocjena svojih učenika
+
+Ravnatelj
+Metode: Konstruktor, Metoda za dodavanje učitelja i studenata, Metoda za uklanjanje učitelja i studenata, Metoda za pregled općih ocjena škole
+
+Razred:
+Atributi: Naziv razreda, Lista studenata
+Metode: Konstruktor, Metoda za dodavanje i uklanjanje studenata, Metoda za prikazivanje ocjena razreda
+
+Scenariji Uporabe:
+Unos Studenata:
+Ravnatelj može dodavati nove studente u sustav.
+
+Unos Učitelja:
+Ravnatelj može dodavati nove učitelje u sustav.
+
+Dodavanje Studenata u Razred:
+Ravnatelj može dodavati studente u razrede.
+
+Dodavanje Ocjena:
+Učitelj može dodavati ocjene studentima.
+
+Pregled Ocjena:
+Student može pregledavati svoje ocjene.
+Učitelj može pregledavati ocjene svojih učenika.
+
+Pregled Ocjena Razreda:
+Učitelj može pregledavati ocjene cijelog razreda.
+
+Pregled Općih Ocjena Škole:
+Ravnatelj može pregledavati ukupne ocjene škole.
+
+Preporuke:
+Korištenje kolekcija poput List<T> za pohranu studenata i ocjena.
+Implementacija autentikacije i autorizacije za pristup različitim dijelovima sustava.
+Osigurajte čitljivost koda, pridržavajte se principa SOLID dizajna te koristite dobro organiziranu i strukturiranu OOP praksu.
